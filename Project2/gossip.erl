b7640fd65b4b4400e0ec5e13bf3a443831d153a9
uDivy <upadhyaydivy10@gmail.com>
2022-10-05 12:00:02 -0400

    The code added at Oct5

--- Project2/gossip.erl
+++ Project2/gossip.erl
@@ -1,19 +1,27 @@
 -module(gossip).
 -compile(export_all).
--define (LineTopology, []).
+-import(string,[concat/2]). 
 
 build_topology(Types) ->
     case Types of
         {line, NumNodes} when NumNodes > 0 ->
             LineTopology = build_line(NumNodes, []),
-            io:format("The Line Topology is: ~p~n",[LineTopology]);
+            io:format("The Line Topology is: ~p~n",[LineTopology]),
+            Start = ErlangSystemTime = erlang:system_time(second),
+            you_know_what_line(length(LineTopology), NumNodes, LineTopology),
+            End = ErlangSystemTime = erlang:system_time(second),
+            io:format("Time Take by Line Topology for ~w Nodes is: ~w seconds~n", [NumNodes, End-Start]);
         {twodgrid, NumNodes} when NumNodes > 0 -> 
             Col = erlang:list_to_integer(erlang:float_to_list(math:sqrt(NumNodes),[{decimals,0}])),
             TwoDTopology = build_twodgrid(NumNodes, Col, []),
             io:format("The 2D Grid Topology is: ~p~n",[TwoDTopology]);
         {fullnw, NumNodes} when NumNodes > 0 -> 
             FullNWTopology = build_fullnw(NumNodes, #{}),
-            io:format("The Full Network Topology is: ~p~n",[FullNWTopology]);
+            io:format("The Full Network Topology is: ~p~n",[maps:find("Neighbour",FullNWTopology)]),
+            Start = ErlangSystemTime = erlang:system_time(second),
+            you_know_what_fullnw(maps:get("Neighbour",FullNWTopology)),
+            End = ErlangSystemTime = erlang:system_time(second),
+            io:format("Time Take by Full Network Topology for ~w Nodes is: ~w seconds~n", [NumNodes, End-Start]);
         {impthreed, NumNodes} when NumNodes > 0 -> 
             Col = erlang:list_to_integer(erlang:float_to_list(math:sqrt(NumNodes),[{decimals,0}])),
             ThreeDTopology = build_impthreed(NumNodes, Col, []),
@@ -22,27 +30,34 @@ build_topology(Types) ->
             unknown
     end.
 
-listen() ->
+listen(Count) ->
     receive
         bye ->
-            io:format("Line closed~n", []);
+            io:format("Line closed~n", []),
+            exit(self());
         {Msg, Sender_id} -> 
-            io:format("My name is ~w and I heard ~p from ~w~n",[self(), Msg, Sender_id]),
-            Sender_id ! ack,
-            listen()
-    end.
+            % io:format("My name is ~w and I heard ~p from ~w and the count is ~w~n",[self(), Msg, Sender_id, Count]),
+            Sender_id ! ack;
+        {Gossip, Sender_id, connect} ->
+            % io:format("My name is ~w and I heard ~p from ~w ~w times~n",[self(), Gossip, Sender_id, Count]),
+            Sender_id ! {really, Count}
+    end,
+    listen(Count + 1).
     
-
+%% BUILD TOPOLOGY
 build_line(0, LineTopology)  ->
     LineTopology;
 build_line(NumNodes, Rest) ->
-    Pid = spawn(gossip, listen, []),
-    Pid ! {"Nothing", self()},
+
+    Str = "human" ++ integer_to_list(NumNodes),
+    register(list_to_atom(Str), spawn(gossip, listen, [0])),
+    list_to_atom(Str) ! {"Nothing", self()},
     receive
     ack -> 
-        io:format("The new member in topology is: ~w~n",[Pid])
+        % io:format("The new member in topology is: ~w~n",[Pid])
+        io:format("",[])
     end,
-    build_line(NumNodes-1, [Pid | Rest]).
+    build_line(NumNodes-1, [list_to_atom(Str) | Rest]).
 
 
 build_twodgrid(NumNodes, Col, TwoDTopology) when NumNodes =< Col->
@@ -64,3 +79,100 @@ build_impthreed(NumNodes, Col, ThreeDTopology) when NumNodes =< Col->
 build_impthreed(NumNodes, Col, Rest) when NumNodes >= Col ->
     Record = build_line(Col, []),
     build_impthreed(NumNodes-Col, Col, [Record | Rest]).
+
+%% Line Topology
+you_know_what_line(_, _, []) ->
+    done;
+you_know_what_line(NumNodes, Pos, LineTopology) when Pos == NumNodes ->
+    % select a neighbour
+    % tell the rumour
+    % get the count of the no. of time, heard the rumour
+    Heardby = lists:nth(Pos, LineTopology),
+    Heardby ! {"youknowwhat", self(), connect},
+    receive
+    {really, Count} ->
+        io:format("Gossip heard by: ~w, ~w no. of times~n",[Heardby, Count]),
+    if Count >= 10 ->
+        Heardby ! bye,
+        LineTopologyNew = lists:delete(Heardby, LineTopology);
+        % io:format("Deleted: ~w~n",[LineTopologyNew]);
+    true ->
+        LineTopologyNew = LineTopology
+    end
+    end,
+    you_know_what_line(length(LineTopologyNew), Pos-1, LineTopologyNew);
+you_know_what_line(NumNodes, Pos, LineTopology) when NumNodes == 1 ->
+    % select a neighbour
+    % tell the rumour
+    % get the count of the no. of time, heard the rumour
+    Heardby = lists:nth(1, LineTopology),
+    Heardby ! {"youknowwhat", self(), connect},
+    receive
+    {really, Count} ->
+        io:format("Gossip heard by: ~w, ~w no. of times~n",[Heardby, Count]),
+    if Count >= 10 ->
+        Heardby ! bye,
+        LineTopologyNew = lists:delete(Heardby, LineTopology);
+        % io:format("Deleted: ~w~n",[LineTopologyNew]);
+    true ->
+        LineTopologyNew = LineTopology
+    end
+    end,
+    you_know_what_line(length(LineTopologyNew), 0, LineTopologyNew);
+
+you_know_what_line(NumNodes, Pos, LineTopology) when Pos > NumNodes div 2 ->
+   % select a neighbour
+    % tell the rumour
+    % get the count of the no. of time, heard the rumour
+    Heardby = lists:nth(Pos, LineTopology),
+    Heardby ! {"youknowwhat", self(), connect},
+    receive
+    {really, Count} ->
+        io:format("Gossip heard by: ~w, ~w no. of times~n",[Heardby, Count]),
+    if Count >= 10 ->
+        Heardby ! bye,
+        LineTopologyNew = lists:delete(Heardby, LineTopology);
+        % io:format("Deleted: ~w~n",[LineTopologyNew]);
+    true ->
+        LineTopologyNew = LineTopology
+    end
+    end,
+you_know_what_line(length(LineTopologyNew), Pos-1, LineTopologyNew);
+
+you_know_what_line(NumNodes, Pos, LineTopology) when Pos =< NumNodes div 2 ->
+   % select a neighbour
+    % tell the rumour
+    % get the count of the no. of time, heard the rumour
+    Heardby = lists:nth(Pos, LineTopology),
+    Heardby ! {"youknowwhat", self(), connect},
+    receive
+    {really, Count} ->
+        io:format("Gossip heard by: ~w, ~w no. of times~n",[Heardby, Count]),
+    if Count >= 10 ->
+        Heardby ! bye,
+        LineTopologyNew = lists:delete(Heardby, LineTopology);
+        % io:format("Deleted: ~w~n",[LineTopologyNew]);
+    true ->
+        LineTopologyNew = LineTopology
+    end
+    end,
+you_know_what_line(length(LineTopologyNew), Pos+1, LineTopologyNew).
+
+%% FULL NW
+you_know_what_fullnw([]) ->
+    done;
+you_know_what_fullnw(FullNWTopology)->
+    Heardby = lists:nth(rand:uniform(length(FullNWTopology)),FullNWTopology),
+    Heardby ! {"youknowwhat", self(), connect},
+    receive
+    {really, Count} ->
+        io:format("Gossip heard by: ~w, ~w no. of times~n",[Heardby, Count]),
+    if Count >= 10 ->
+        Heardby ! bye,
+        FullNWTopologyNew = lists:delete(Heardby, FullNWTopology);
+        % io:format("Deleted: ~w~n",[LineTopologyNew]);
+    true ->
+        FullNWTopologyNew = FullNWTopology
+    end
+    end,
+you_know_what_fullnw(FullNWTopologyNew).
