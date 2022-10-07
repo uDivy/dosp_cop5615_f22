6b44eead36dac7442f89e48fb07e771ba322ea01
uDivy <upadhyaydivy10@gmail.com>
2022-10-07 03:10:37 -0400

    The code added at Oct7

--- Project2/gossip2.erl
+++ Project2/gossip2.erl
@@ -18,14 +18,36 @@ build_topology(Types) ->
             unknown
     end.
 
+random_selection_of_living_actors(Curprocess, Count, Sender_id, [First | Rest], Num) ->
+    if Count == Num ->
+        [Count,0];
+    true -> 
+        if First == Curprocess ->
+            random_selection_of_living_actors(Curprocess, Count+1, Sender_id, Rest, Num);
+        true ->
+            Status = is_process_alive(First),
+            if Status == false ->
+                random_selection_of_living_actors(Curprocess, Count+1, Sender_id, Rest, Num);
+            true ->
+                [Count, First]
+            end
+        end
+    end.
+    
+
+
+
+
 listen(Count) ->
     receive
         {Msg, Sender_id} -> 
             % io:format("My name is ~w and I heard ~p from ~w and the count is ~w~n",[self(), Msg, Sender_id, Count]),
             Sender_id ! ack;
-        {Gossip, Sender_id, Neighbour, Pos, connectline} ->
+        {Gossip, Sender_id, Neighbour, Pos, LineTopology, connectline} ->
             if Count > 10 ->
-                Sender_id ! done,
+                % Sender_id ! done,
+                io:format("MY name is ~w and I heard ~p from ~w ~w times~n",[self(), Gossip, Sender_id, Count]),
+                lists:nth(2,random_selection_of_living_actors(self(), 1, Sender_id, LineTopology, length(LineTopology))) ! {"youknowwhat", Sender_id, Neighbour, lists:nth(1,random_selection_of_living_actors(self(), 1, Sender_id, LineTopology, length(LineTopology))), LineTopology, connectline},
                 exit(self());
             true ->
                 io:format("My name is ~w and I heard ~p from ~w ~w times~n",[self(), Gossip, Sender_id, Count]),
@@ -41,9 +63,11 @@ listen(Count) ->
                     Posnew = Pos + 1
                 end,
                 Heardby = lists:nth(Coln, Blockof),
-                case whereis(Heardby) of 
-                    undefined -> listen(Count);
-                    _ -> Heardby ! {"youknowwhat", Sender_id, Neighbour, Posnew, connectline}
+                io:format("Lets see ~w ~w ~n",[is_process_alive(Heardby), Heardby]),
+                case is_process_alive(Heardby) of 
+                    false -> 
+                        lists:nth(2,random_selection_of_living_actors(self(), 1, Sender_id, LineTopology, length(LineTopology))) ! {"youknowwhat", Sender_id, Neighbour, lists:nth(1,random_selection_of_living_actors(self(), 1, Sender_id, LineTopology, length(LineTopology))), LineTopology, connectline};
+                    _ -> Heardby ! {"youknowwhat", Sender_id, Neighbour, Posnew, LineTopology, connectline}
                 end
             end
     end,
@@ -53,23 +77,24 @@ listen(Count) ->
 build_line(0, LineTopology, _)  ->
     LineTopology;
 build_line(NumNodes, Rest, Row) ->
-    AllowedChars = "qwertyuiopasdfghjklzxcvbnm",
+    % AllowedChars = "qwertyuiopasdfghjklzxcvbnm",
 
-    Ranstring = lists:foldl(fun(_, Acc) ->
-                    [lists:nth(rand:uniform(length(AllowedChars)),
-                                AllowedChars)]
-                              ++ Acc
-                    end, [] , lists:seq(1, 10)),
+    % Ranstring = lists:foldl(fun(_, Acc) ->
+    %                 [lists:nth(rand:uniform(length(AllowedChars)),
+    %                             AllowedChars)]
+    %                           ++ Acc
+    %                 end, [] , lists:seq(1, 10)),
 
-    Str = Ranstring ++ "human" ++ integer_to_list(Row) ++ integer_to_list(NumNodes),
-    register(list_to_atom(Str), spawn(gossip2, listen, [0])),
-    list_to_atom(Str) ! {"Nothing", self()},
+    % Str = Ranstring ++ "human" ++ integer_to_list(Row) ++ integer_to_list(NumNodes),
+    % register(list_to_atom(Str), spawn(gossip2, listen, [0])),
+    Pid = spawn(gossip2, listen, [0]),
+    Pid ! {"Nothing", self()},
     receive
     ack -> 
         % io:format("The new member in topology is: ~w~n",[Pid])
         io:format("",[])
     end,
-    build_line(NumNodes-1, [list_to_atom(Str) | Rest], Row+1).
+    build_line(NumNodes-1, [Pid | Rest], Row+1).
 
 
 await_result(Sender_id, Count) ->
@@ -91,19 +116,22 @@ find_my_neighbour_1d(NumNodes, Pos, LineTopology, Neighbour) ->
 
 you_know_what_line(NumNodes, Pos, LineTopology, Neighbour, mycounter) when Pos == 0 ->
     receive
-        {done} ->
-            mycounter ! {counter, self()};
-        {value, Count} ->
-            if Count == NumNodes ->
-                done;
-            true ->
-                you_know_what_line(NumNodes, Pos, LineTopology, Neighbour, mycounter)
-            end
+        {donedone} ->
+            exit(normal)
+            % io:format("done ~n",[]),
+            % mycounter ! {counter, self()}
+        % {value, Count} ->
+        %     io:format("Count ~n",[]),
+        %     if Count == NumNodes*10 ->
+        %         done;
+        %     true ->
+        %         you_know_what_line(NumNodes, Pos, LineTopology, Neighbour, mycounter)
+        %     end
     end;
 you_know_what_line(NumNodes, Pos, LineTopology,Neighbour, mycounter) when Pos == NumNodes->
     Heardby = lists:nth(Pos, LineTopology),
     % io:format("~w ~n",[Heardby]),
-    Heardby ! {"youknowwhat", self(), Neighbour, Pos,  connectline},
+    Heardby ! {"youknowwhat", self(), Neighbour, Pos,  LineTopology, connectline},
     % receive
     %     ack ->
     %         io:format("Final ~w ~n",[Heardby])
